b67655e03108a4e04e761d04b55c112d5a6cee55
Julio Suárez <jsuarezp1300@alumno.ipn.mx>
2023-04-02 12:15:41 -0600

    Proyecto BlazingPizza

---
+++ BlazingPizza/Server/Models/PizzaStoreContext.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazingPizza.Server.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using BlazingPizza.Shared;
+    public class PizzaStoreContext : DbContext
+    {
+        public DbSet<PizzaSpecial> Specials { get; set; }
+        public PizzaStoreContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
